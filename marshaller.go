6c024a63e4cb89014ea5a341a578c3f074c09c43
Nikita Konev <nikit.cpp@yandex.ru>
2025-04-24 01:23:41 +0300

    minor

--- marshaller.go
+++ marshaller.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/ThreeDotsLabs/watermill-kafka/v3/pkg/kafka"
 	"github.com/ThreeDotsLabs/watermill/components/cqrs"
@@ -23,8 +24,14 @@ func (c CqrsMarshalerDecorator) Unmarshal(msg *message.Message, v interface{}) (
 	}
 
 	partition, ok := kafka.MessagePartitionFromCtx(msg.Context())
+	if !ok {
+		return errors.New("Unable to get partition")
+	}
 
 	offset, ok := kafka.MessagePartitionOffsetFromCtx(msg.Context())
+	if !ok {
+		return errors.New("Unable to get offset")
+	}
 
 	pm.SetOffset(partition, offset)
 
