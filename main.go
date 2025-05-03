242eb28b15e520b47f88b8a9b04298bdb265815e
Nikita Konev <nikit.cpp@yandex.ru>
2025-05-04 02:17:08 +0300

    restore todo

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "main.go/cmd"
 
+// TODO change chat
+// TODO delete chat
+// TODO remove participant from chat
+// TODO consider batching for adding participants
+// TODO draw the last message in user's chat view
+// TODO think about recalculate unreads in case remove message
 func main() {
 	cmd.Execute()
 }
