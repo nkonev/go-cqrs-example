ecd5c469f26380b2dc64fc84c7d9dc52566ec5d7
Nikita Konev <nikit.cpp@yandex.ru>
2025-05-09 06:30:44 +0300

    remove old todos

--- main.go
+++ main.go
@@ -2,12 +2,6 @@ package main
 
 import "main.go/cmd"
 
-// TODO change chat
-// TODO delete chat
-// TODO remove participant from chat
-// TODO consider batching for adding participants
-// TODO draw the last message in user's chat view
-// TODO think about recalculate unreads in case remove message
 func main() {
 	cmd.Execute()
 }
